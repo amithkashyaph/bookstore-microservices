9db7340dce9ba432e55ec1205c85cc205309a548
amithkashyap <amithkashyaprv@gmail.com>
2025-11-12 01:26:13 +0530

    implemented order not found exception

--- order-service/src/main/java/com/microservices/bookstore/order_service/exceptions/OrderNotFoundException.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/exceptions/OrderNotFoundException.java
@@ -1,4 +1,11 @@
 package com.microservices.bookstore.order_service.exceptions;
 
 public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public static OrderNotFoundException forOrderId(Long orderId) {
+        return new OrderNotFoundException("Order with id: " + orderId + " does not exist");
+    }
 }
