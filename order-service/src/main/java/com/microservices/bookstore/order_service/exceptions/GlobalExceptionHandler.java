ec52ba228025e10a0693adb252265d3ef88d5d5f
amithkashyap <amithkashyaprv@gmail.com>
2025-11-12 01:27:17 +0530

    added initial global exception handler

---
+++ order-service/src/main/java/com/microservices/bookstore/order_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.microservices.bookstore.order_service.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
