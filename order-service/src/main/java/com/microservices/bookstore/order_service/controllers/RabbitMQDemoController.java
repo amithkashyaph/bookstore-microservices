e1b717352a07ce88f2149ee25101b1171fc9869a
amithkashyap <amithkashyaprv@gmail.com>
2025-11-14 01:05:56 +0530

    added initial fields in rabbitmq demo controller

--- order-service/src/main/java/com/microservices/bookstore/order_service/controllers/RabbitMQDemoController.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/controllers/RabbitMQDemoController.java
@@ -1,4 +1,18 @@
 package com.microservices.bookstore.order_service.controllers;
 
+import com.microservices.bookstore.order_service.configuration.ApplicationProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RabbitMQDemoController {
+
+    private RabbitTemplate rabbitTemplate;
+    private ApplicationProperties applicationProperties;
+
+
+    RabbitMQDemoController(RabbitTemplate rabbitTemplate, ApplicationProperties applicationProperties) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.applicationProperties = applicationProperties;
+    }
 }
