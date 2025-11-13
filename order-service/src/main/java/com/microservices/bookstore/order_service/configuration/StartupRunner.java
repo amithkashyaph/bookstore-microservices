b746773e0e926b0fdcd9940f3ca7b67269af5e4a
amithkashyap <amithkashyaprv@gmail.com>
2025-11-13 23:53:32 +0530

    added initial startup runner component

---
+++ order-service/src/main/java/com/microservices/bookstore/order_service/configuration/StartupRunner.java
@@ -0,0 +1,10 @@
+package com.microservices.bookstore.order_service.configuration;
+
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+}
