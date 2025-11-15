c785f3a00a37ca22ce748dc2c3a6a2c61d1da80e
amithkashyap <amithkashyaprv@gmail.com>
2025-11-15 23:17:18 +0530

    added logic to initialize rabbitmq admin

--- order-service/src/main/java/com/microservices/bookstore/order_service/configuration/StartupRunner.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/configuration/StartupRunner.java
@@ -7,4 +7,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartupRunner implements CommandLineRunner {
 
+    private final RabbitAdmin rabbitAdmin;
+
+    public StartupRunner(RabbitAdmin rabbitAdmin) {
+        this.rabbitAdmin = rabbitAdmin;
+    }
+
+    @Override
+    public void run(String... args) {
+        rabbitAdmin.initialize(); // forces declaration
+    }
 }
