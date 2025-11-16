31c682fe121ce1ff015da0318610ab43186e1a20
amithkashyap <amithkashyaprv@gmail.com>
2025-11-16 23:18:47 +0530

    added order entity

---
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/Order.java
@@ -0,0 +1,23 @@
+package com.microservices.bookstore.order_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
