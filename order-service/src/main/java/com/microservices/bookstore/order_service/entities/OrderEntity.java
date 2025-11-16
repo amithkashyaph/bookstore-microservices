304502c14cc67fd6e589c042d6e7c4d10f5fb8f4
amithkashyap <amithkashyaprv@gmail.com>
2025-11-16 23:07:52 +0530

    annotated order entity

--- order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderEntity.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderEntity.java
@@ -1,4 +1,23 @@
 package com.microservices.bookstore.order_service.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
 }
