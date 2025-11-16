c9f3e96278cfa3fee10f10caf0d215d7b3bc5b22
amithkashyap <amithkashyaprv@gmail.com>
2025-11-16 23:21:43 +0530

    added initial fields in order item entity

--- order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderItem.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderItem.java
@@ -1,4 +1,15 @@
 package com.microservices.bookstore.order_service.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
