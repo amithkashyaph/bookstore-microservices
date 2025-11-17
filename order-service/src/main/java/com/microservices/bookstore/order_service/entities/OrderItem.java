9248a80096885827133fd1f1ca4e89228d6c9862
amithkashyap <amithkashyaprv@gmail.com>
2025-11-17 19:55:09 +0530

    added additional fields order item entity

--- order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderItem.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderItem.java
@@ -1,13 +1,30 @@
 package com.microservices.bookstore.order_service.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
-public class OrderItem {
-
+@Getter
+@Setter
+@Table(name = "order_items")
+class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
+    private String code;
+
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer quantity;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "order_id")
