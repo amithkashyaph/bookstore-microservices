c901c2c6a007e3a19731ec1c7aedeeabb4e844a6
amithkashyap <amithkashyaprv@gmail.com>
2025-11-18 22:35:37 +0530

    added fields in paged result record

--- catalog-service/src/main/java/com/microservices/bookstore/catalog_service/dtos/PagedResult.java
+++ catalog-service/src/main/java/com/microservices/bookstore/catalog_service/dtos/PagedResult.java
@@ -2,4 +2,12 @@ package com.microservices.bookstore.catalog_service.dtos;
 
 import java.util.List;
 
-public record PagedResult<T>() {}
+public record PagedResult<T>(
+        List<T> data,
+        long totalElements,
+        int pageNumber,
+        int totalPages,
+        boolean isFirst,
+        boolean isLast,
+        boolean hasNext,
+        boolean hasPrevious) {}
