c6000dac46d19c7baf0ef84c1613c6e2a801e772
amithkashyap <amithkashyaprv@gmail.com>
2025-11-19 23:19:58 +0530

    added fields in address record

--- order-service/src/main/java/com/microservices/bookstore/order_service/entities/embeddable_records/Address.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/embeddable_records/Address.java
@@ -1,4 +1,12 @@
 package com.microservices.bookstore.order_service.entities.embeddable_records;
 
-public record Address() {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record Address(
+        @NotBlank(message = "AddressLine1 is required") String addressLine1,
+        String addressLine2,
+        @NotBlank(message = "City is required") String city,
+        @NotBlank(message = "State is required") String state,
+        @NotBlank(message = "ZipCode is required") String zipCode,
+        @NotBlank(message = "Country is required") String country
+) {}
