6559de62293e382c4119ebe918a535a7f7bf7bcf
amithkashyap <amithkashyaprv@gmail.com>
2025-11-20 18:23:39 +0530

    added embeddable customer entity field in order entity

--- order-service/src/main/java/com/microservices/bookstore/order_service/entities/Order.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/Order.java
@@ -1,11 +1,15 @@
 package com.microservices.bookstore.order_service.entities;
 
+import com.microservices.bookstore.order_service.entities.embeddable_records.Address;
+import com.microservices.bookstore.order_service.entities.embeddable_records.Customer;
+import com.microservices.bookstore.order_service.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
@@ -28,5 +32,16 @@ public class Order {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "order")
     private Set<OrderItem> orderItems;
+
+    @Embedded
+    @AttributeOverrides(
+            value = {
+                    @AttributeOverride(name = "name", column = @Column(name = "customer_name")),
+                    @AttributeOverride(name = "email", column = @Column(name = "customer_email")),
+                    @AttributeOverride(name = "phone", column = @Column(name = "customer_phone"))
+            })
+    private Customer customer;
+
+
 
 }
