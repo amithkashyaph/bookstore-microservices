51fd6136a8395a699bc58e8838e10cd6de3eee15
amithkashyap <amithkashyaprv@gmail.com>
2025-11-27 18:11:06 +0530

    initialized required beans

--- order-service/src/main/java/com/microservices/bookstore/order_service/controllers/OrderController.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/controllers/OrderController.java
@@ -1,5 +1,7 @@
 package com.microservices.bookstore.order_service.controllers;
 
+import com.microservices.bookstore.order_service.services.OrderService;
+import com.microservices.bookstore.order_service.services.SecurityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,4 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/orders")
 public class OrderController {
     private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
+    private OrderService orderService;
+    private SecurityService securityService;
+
+    public OrderController(OrderService orderService, SecurityService securityService) {
+        this.orderService = orderService;
+        this.securityService = securityService;
+    }
 }
