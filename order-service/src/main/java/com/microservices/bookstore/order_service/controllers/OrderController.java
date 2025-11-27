ac122301bf4c6bdf2ab4f3ca8af0a31e159402a0
amithkashyap <amithkashyaprv@gmail.com>
2025-11-27 18:09:48 +0530

    added initial order controller

--- order-service/src/main/java/com/microservices/bookstore/order_service/controllers/OrderController.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/controllers/OrderController.java
@@ -1,4 +1,12 @@
 package com.microservices.bookstore.order_service.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 }
