08aac5d1159b96d86147ed63580df5af691b46e6
amithkashyap <amithkashyaprv@gmail.com>
2025-11-27 19:11:01 +0530

    added initial order controller test class

---
+++ order-service/src/test/java/com/microservices/bookstore/order_service/controllers/OrderControllerTests.java
@@ -0,0 +1,6 @@
+package com.microservices.bookstore.order_service.controllers;
+
+import com.microservices.bookstore.order_service.AbstractIntegrationTest;
+
+public class OrderControllerTests extends AbstractIntegrationTest {
+}
