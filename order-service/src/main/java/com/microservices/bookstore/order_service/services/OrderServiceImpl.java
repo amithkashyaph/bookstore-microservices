9b307d7616d46def94a4845804d0bc5d3ca82c87
amithkashyap <amithkashyaprv@gmail.com>
2025-11-27 18:16:00 +0530

    added initial implementation of order service method

--- order-service/src/main/java/com/microservices/bookstore/order_service/services/OrderServiceImpl.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/OrderServiceImpl.java
@@ -1,4 +1,14 @@
 package com.microservices.bookstore.order_service.services;
 
-public class OrderServiceImpl {
+import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
+import com.microservices.bookstore.order_service.dtos.CreateOrderResponse;
+import com.microservices.bookstore.order_service.services.interfaces.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public CreateOrderResponse createOrder(String username, CreateOrderRequest createOrderRequest) {
+        return null;
+    }
 }
