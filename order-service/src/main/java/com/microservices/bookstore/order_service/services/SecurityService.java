0a88798a21ea6bdc1d24614cd0290f2ceba68ab6
amithkashyap <amithkashyaprv@gmail.com>
2025-11-27 18:06:51 +0530

    added simple security service implementation

--- order-service/src/main/java/com/microservices/bookstore/order_service/services/SecurityService.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/SecurityService.java
@@ -1,5 +1,11 @@
 package com.microservices.bookstore.order_service.services;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class SecurityService {
+
+    public String getLoggedInUserName() {
+        return "user";
+    }
 }
