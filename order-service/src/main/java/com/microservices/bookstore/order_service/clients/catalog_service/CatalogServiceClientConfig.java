d677862dea329282b134af66440db9e480ff9a86
amithkashyap <amithkashyaprv@gmail.com>
2025-11-28 22:51:57 +0530

    implemented catalog service client config class

--- order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/CatalogServiceClientConfig.java
@@ -1,4 +1,17 @@
 package com.microservices.bookstore.order_service.clients.catalog_service;
 
+import com.microservices.bookstore.order_service.configuration.ApplicationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
 public class CatalogServiceClientConfig {
+
+    @Bean
+    RestClient restClient(ApplicationProperties applicationProperties) {
+        return RestClient.builder()
+                .baseUrl(applicationProperties.catalogServiceUrl())
+                .build();
+    }
 }
