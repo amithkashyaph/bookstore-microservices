472516d901227f145bd7df6eed2cc1bddf3d1f5b
amithkashyap <amithkashyaprv@gmail.com>
2025-11-30 23:36:45 +0530

    initialized restclient within catalog service client

--- order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/CatalogServiceClient.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/CatalogServiceClient.java
@@ -1,4 +1,17 @@
 package com.microservices.bookstore.order_service.clients.catalog_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
 public class CatalogServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(CatalogServiceClient.class);
+
+    private final RestClient restClient;
+
+    public CatalogServiceClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
 }
