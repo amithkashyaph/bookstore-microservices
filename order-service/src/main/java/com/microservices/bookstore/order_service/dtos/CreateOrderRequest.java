8a663769b8ec703a78105f74c75815c0862ad5fc
amithkashyap <amithkashyaprv@gmail.com>
2025-12-01 21:40:17 +0530

    implemented record for create order request

--- order-service/src/main/java/com/microservices/bookstore/order_service/dtos/CreateOrderRequest.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/dtos/CreateOrderRequest.java
@@ -1,4 +1,14 @@
 package com.microservices.bookstore.order_service.dtos;
 
-public record CreateOrderRequest() {
-}
+import com.microservices.bookstore.order_service.entities.embeddable_records.Address;
+import com.microservices.bookstore.order_service.entities.embeddable_records.Customer;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.Set;
+
+public record CreateOrderRequest(
+        @Valid @NotEmpty(message = "Items cannot be empty") Set<OrderItemDto> items,
+        @Valid Customer customer,
+        @Valid Address deliveryAddress
+) {}
