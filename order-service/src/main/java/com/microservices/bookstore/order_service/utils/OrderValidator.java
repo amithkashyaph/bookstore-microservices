cd2d38ac34f3266a015b97c3a9ba5245a42b1c4a
amithkashyap <amithkashyaprv@gmail.com>
2025-12-01 21:47:49 +0530

    initialized order validator class

--- order-service/src/main/java/com/microservices/bookstore/order_service/utils/OrderValidator.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/utils/OrderValidator.java
@@ -1,4 +1,18 @@
 package com.microservices.bookstore.order_service.utils;
 
+import com.microservices.bookstore.order_service.clients.catalog_service.CatalogServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderValidator {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderValidator.class);
+
+    private final CatalogServiceClient catalogServiceClient;
+
+    public OrderValidator(CatalogServiceClient catalogServiceClient) {
+        this.catalogServiceClient = catalogServiceClient;
+    }
 }
