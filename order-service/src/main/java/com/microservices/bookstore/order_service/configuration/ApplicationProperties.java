90bd890867660546909a2ab75e07b4f9359abdee
amithkashyap <amithkashyaprv@gmail.com>
2025-12-04 18:51:56 +0530

    added fields in app properties class

--- order-service/src/main/java/com/microservices/bookstore/order_service/configuration/ApplicationProperties.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/configuration/ApplicationProperties.java
@@ -3,5 +3,12 @@ package com.microservices.bookstore.order_service.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "orders")
-public record ApplicationProperties() {
+public record ApplicationProperties(
+        String catalogServiceUrl,
+        String orderEventsExchange,
+        String newOrdersQueue,
+        String deliveredOrdersQueue,
+        String cancelledOrdersQueue,
+        String errorOrdersQueue
+) {
 }
