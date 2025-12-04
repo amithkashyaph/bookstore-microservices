6dcde900a994ef4cdd11deb1ba657c995b065063
amithkashyap <amithkashyaprv@gmail.com>
2025-12-04 18:53:00 +0530

    added order service interface

--- order-service/src/main/java/com/microservices/bookstore/order_service/services/interfaces/OrderService.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/interfaces/OrderService.java
@@ -1,5 +1,9 @@
 package com.microservices.bookstore.order_service.services.interfaces;
 
+import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
+import com.microservices.bookstore.order_service.dtos.CreateOrderResponse;
+
 public interface OrderService {
+    CreateOrderResponse createOrder(String username, CreateOrderRequest createOrderRequest);
 
 }
