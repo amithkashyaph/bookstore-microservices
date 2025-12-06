b5cddb5a104e1ef2a5e0e0af65fe4f9a25df4850
amithkashyap <amithkashyaprv@gmail.com>
2025-12-06 13:42:44 +0530

    added fields

--- order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/dtos/ProductDto.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/dtos/ProductDto.java
@@ -1,4 +1,12 @@
 package com.microservices.bookstore.order_service.clients.catalog_service.dtos;
 
-public record ProductDto() {
+import java.math.BigDecimal;
+
+public record ProductDto(
+        String code,
+        String name,
+        String description,
+        String imageUrl,
+        BigDecimal price
+) {
 }
