2492c9ba19c72a7435f7c7b7a1362a0922676448
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 17:45:58 +0530

    initialized fields in publisher class

--- order-service/src/main/java/com/microservices/bookstore/order_service/queues/OrderEventPublisher.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/queues/OrderEventPublisher.java
@@ -1,10 +1,20 @@
 package com.microservices.bookstore.order_service.queues;
 
+import com.microservices.bookstore.order_service.configuration.ApplicationProperties;
 import com.microservices.bookstore.order_service.dtos.OrderCreatedEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final ApplicationProperties applicationProperties;
+
+    public OrderEventPublisher(RabbitTemplate rabbitTemplate, ApplicationProperties applicationProperties) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.applicationProperties = applicationProperties;
+    }
     public void publish(OrderCreatedEvent orderCreatedEvent) {
     }
 }
