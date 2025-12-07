eea641b8487bf861d67e54ccfae0da2748b0a9ed
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 17:28:12 +0530

    initialized order events publishing job class

--- order-service/src/main/java/com/microservices/bookstore/order_service/jobs/OrderEventsPublishingJob.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/jobs/OrderEventsPublishingJob.java
@@ -1,4 +1,18 @@
 package com.microservices.bookstore.order_service.jobs;
 
+import com.microservices.bookstore.order_service.services.OrderEventServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderEventsPublishingJob {
+    private static final Logger log = LoggerFactory.getLogger(OrderEventsPublishingJob.class);
+
+    private final OrderEventServiceImpl orderEventService;
+
+    OrderEventsPublishingJob(OrderEventServiceImpl orderEventService) {
+        this.orderEventService = orderEventService;
+    }
+
 }
