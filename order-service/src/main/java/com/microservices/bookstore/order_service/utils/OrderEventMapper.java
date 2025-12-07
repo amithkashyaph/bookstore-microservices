66ec6cfe34cca42edbcd67b5c126edb0d421ebe5
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 12:24:33 +0530

    added initial order event mapper class

---
+++ order-service/src/main/java/com/microservices/bookstore/order_service/utils/OrderEventMapper.java
@@ -0,0 +1,14 @@
+package com.microservices.bookstore.order_service.utils;
+
+import com.microservices.bookstore.order_service.dtos.OrderCreatedEvent;
+import com.microservices.bookstore.order_service.dtos.OrderItemDto;
+import com.microservices.bookstore.order_service.entities.Order;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class OrderEventMapper {
+
+}
