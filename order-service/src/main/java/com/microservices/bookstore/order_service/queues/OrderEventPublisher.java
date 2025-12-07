c7c55a094da3cfac56e257bc64e9de2b53221d13
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 17:43:43 +0530

    added initial publish method

--- order-service/src/main/java/com/microservices/bookstore/order_service/queues/OrderEventPublisher.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/queues/OrderEventPublisher.java
@@ -1,4 +1,10 @@
 package com.microservices.bookstore.order_service.queues;
 
+import com.microservices.bookstore.order_service.dtos.OrderCreatedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderEventPublisher {
+    public void publish(OrderCreatedEvent orderCreatedEvent) {
+    }
 }
