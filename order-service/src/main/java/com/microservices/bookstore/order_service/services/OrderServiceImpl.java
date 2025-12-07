7876e67560a4f35aaf217bdf65e51e6ec2d9aa6c
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 20:34:57 +0530

    added orderEventService

--- order-service/src/main/java/com/microservices/bookstore/order_service/services/OrderServiceImpl.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/OrderServiceImpl.java
@@ -2,9 +2,11 @@ package com.microservices.bookstore.order_service.services;
 
 import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
 import com.microservices.bookstore.order_service.dtos.CreateOrderResponse;
+import com.microservices.bookstore.order_service.dtos.OrderCreatedEvent;
 import com.microservices.bookstore.order_service.entities.Order;
 import com.microservices.bookstore.order_service.repositories.OrderRepository;
 import com.microservices.bookstore.order_service.services.interfaces.OrderService;
+import com.microservices.bookstore.order_service.utils.OrderEventMapper;
 import com.microservices.bookstore.order_service.utils.OrderMapper;
 import com.microservices.bookstore.order_service.utils.OrderValidator;
 import org.slf4j.Logger;
@@ -17,10 +19,12 @@ public class OrderServiceImpl implements OrderService {
     private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
     private final OrderRepository orderRepository;
     private final OrderValidator orderValidator;
+    private final OrderEventServiceImpl orderEventService;
 
-    OrderServiceImpl(OrderRepository orderRepository, OrderValidator orderValidator) {
+    OrderServiceImpl(OrderRepository orderRepository, OrderValidator orderValidator, OrderEventServiceImpl orderEventService) {
         this.orderRepository = orderRepository;
         this.orderValidator = orderValidator;
+        this.orderEventService = orderEventService;
     }
     @Override
     public CreateOrderResponse createOrder(String username, CreateOrderRequest createOrderRequest) {
@@ -30,6 +34,9 @@ public class OrderServiceImpl implements OrderService {
         Order savedOrder = orderRepository.save(newOrder);
 
         log.info("Created order with orderNumber = {}", savedOrder.getOrderNumber());
+
+        OrderCreatedEvent orderCreatedEvent = OrderEventMapper.buildOrderCreatedEvent(savedOrder);
+        orderEventService.save(orderCreatedEvent);
 
         return new CreateOrderResponse(savedOrder.getOrderNumber());
     }
