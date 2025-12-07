2522f1d32d2488c54a2a87293ac8b5adbff64db1
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 20:41:10 +0530

    added findByStatus method

--- order-service/src/main/java/com/microservices/bookstore/order_service/repositories/OrderRepository.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/repositories/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.microservices.bookstore.order_service.repositories;
 
 import com.microservices.bookstore.order_service.entities.Order;
+import com.microservices.bookstore.order_service.entities.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByStatus(OrderStatus orderStatus);
 }
