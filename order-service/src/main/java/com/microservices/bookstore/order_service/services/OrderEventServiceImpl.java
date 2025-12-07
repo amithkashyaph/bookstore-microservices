10bb2ac382126f31a3161ac9a564db03c13821a5
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 12:28:15 +0530

    initialized fields in order event service class

--- order-service/src/main/java/com/microservices/bookstore/order_service/services/OrderEventServiceImpl.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/OrderEventServiceImpl.java
@@ -9,6 +9,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderEventServiceImpl {
+    private static final Logger log = LoggerFactory.getLogger(OrderEventServiceImpl.class);
+    private final OrderEventRepository orderEventRepository;
+    private final OrderEventPublisher orderEventPublisher;
 
+    private final ObjectMapper objectMapper;
+
+    public OrderEventServiceImpl(OrderEventRepository orderEventRepository, OrderEventPublisher orderEventPublisher, ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.orderEventPublisher = orderEventPublisher;
+        this.orderEventRepository = orderEventRepository;
+    }
 
 }
