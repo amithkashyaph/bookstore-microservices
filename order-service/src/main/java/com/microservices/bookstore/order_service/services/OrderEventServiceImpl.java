220176da5dd821cc4f73a51266f223462b162540
amithkashyap <amithkashyaprv@gmail.com>
2025-12-07 12:27:48 +0530

    added initial order event service class

---
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/OrderEventServiceImpl.java
@@ -0,0 +1,14 @@
+package com.microservices.bookstore.order_service.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservices.bookstore.order_service.queues.OrderEventPublisher;
+import com.microservices.bookstore.order_service.repositories.OrderEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderEventServiceImpl {
+
+
+}
