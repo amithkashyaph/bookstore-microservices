9caef43efcf14ed00e5e6c0eddac34bc4a5261b8
amithkashyap <amithkashyaprv@gmail.com>
2025-12-08 18:00:43 +0530

    enabled scheduling on main class

--- order-service/src/main/java/com/microservices/bookstore/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/OrderServiceApplication.java
@@ -1,12 +1,15 @@
 package com.microservices.bookstore.order_service;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
