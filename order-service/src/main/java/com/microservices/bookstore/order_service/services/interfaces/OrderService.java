2c6aeb8db998a3b7f571890332789135a0ff0f48
amithkashyap <amithkashyaprv@gmail.com>
2025-12-09 19:20:34 +0530

    added fetchAllOrdersForUser interface method

--- order-service/src/main/java/com/microservices/bookstore/order_service/services/interfaces/OrderService.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/interfaces/OrderService.java
@@ -2,8 +2,12 @@ package com.microservices.bookstore.order_service.services.interfaces;
 
 import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
 import com.microservices.bookstore.order_service.dtos.CreateOrderResponse;
+import com.microservices.bookstore.order_service.dtos.OrderSummary;
+
+import java.util.List;
 
 public interface OrderService {
     CreateOrderResponse createOrder(String username, CreateOrderRequest createOrderRequest);
+    List<OrderSummary> fetchAllOrdersForUser(String username);
 
 }
