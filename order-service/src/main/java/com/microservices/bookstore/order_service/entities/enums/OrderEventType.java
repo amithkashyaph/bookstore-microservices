0b3b12199897b01ca0be1506576fd5bf31c43786
amithkashyap <amithkashyaprv@gmail.com>
2025-12-12 18:48:23 +0530

    added order event type enum

--- order-service/src/main/java/com/microservices/bookstore/order_service/entities/enums/OrderEventType.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/enums/OrderEventType.java
@@ -1,4 +1,8 @@
 package com.microservices.bookstore.order_service.entities.enums;
 
 public enum OrderEventType {
+    ORDER_CREATED,
+    ORDER_DELIVERED,
+    ORDER_CANCELLED,
+    ORDER_PROCESSING_FAILED
 }
