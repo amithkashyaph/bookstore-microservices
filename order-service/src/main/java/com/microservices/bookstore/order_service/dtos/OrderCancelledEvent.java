2e9d8d397ca101765c423be225fd978a502bcc62
amithkashyap <amithkashyaprv@gmail.com>
2025-12-12 18:47:56 +0530

    added order cancelled event dto

--- order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderCancelledEvent.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderCancelledEvent.java
@@ -1,4 +1,18 @@
 package com.microservices.bookstore.order_service.dtos;
 
-public record OrderCancelledEvent() {
+import com.microservices.bookstore.order_service.entities.embeddable_records.Address;
+import com.microservices.bookstore.order_service.entities.embeddable_records.Customer;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record OrderCancelledEvent(
+        String eventId,
+        String orderNumber,
+        Set<OrderItemDto> items,
+        Customer customer,
+        Address deliveryAddress,
+        String reason,
+        LocalDateTime createdAt
+) {
 }
