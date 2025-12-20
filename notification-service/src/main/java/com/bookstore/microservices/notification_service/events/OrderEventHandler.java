6f57740b4016ee8731a4799ca14ab33df57f7d74
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 15:12:22 +0530

    added notification service dependency

--- notification-service/src/main/java/com/bookstore/microservices/notification_service/events/OrderEventHandler.java
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/events/OrderEventHandler.java
@@ -4,29 +4,40 @@ import com.bookstore.microservices.notification_service.entities.OrderCancelledE
 import com.bookstore.microservices.notification_service.entities.OrderCreatedEvent;
 import com.bookstore.microservices.notification_service.entities.OrderDeliveredEvent;
 import com.bookstore.microservices.notification_service.entities.OrderErrorEvent;
+import com.bookstore.microservices.notification_service.service.NotificationService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventHandler {
 
+    private final NotificationService notificationService;
+
+    public OrderEventHandler(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     @RabbitListener(queues = "${notifications.new-orders-queue}")
     void handleOrderCreatedEvent(OrderCreatedEvent event) {
         System.out.println("Order created event : " + event);
+        notificationService.sendOrderCreatedNotification(event);
     }
 
     @RabbitListener(queues = "${notifications.delivered-orders-queue}")
     void handleOrderDeliveredEvent(OrderDeliveredEvent event) {
         System.out.println("Order delivered event : " + event);
+        notificationService.sendOrderDeliveredNotification(event);
     }
 
     @RabbitListener(queues = "${notifications.cancelled-orders-queue}")
     void handleOrderCancelledEvent(OrderCancelledEvent event) {
         System.out.println("Order cancelled event : " + event);
+        notificationService.sendOrderCancelledNotification(event);
     }
 
     @RabbitListener(queues = "${notifications.error-orders-queue}")
     void handleOrderErrorEvent(OrderErrorEvent event) {
         System.out.println("Order error event : " + event);
+        notificationService.sendOrderErrorEventNotification(event);
     }
 }
