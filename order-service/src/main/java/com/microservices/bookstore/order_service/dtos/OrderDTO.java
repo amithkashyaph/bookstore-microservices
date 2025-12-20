c1019c0b4f7b25d93a8b7ba5e866d3112b8e6bc1
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 12:26:27 +0530

    added fields in order dto record

--- order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderDTO.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderDTO.java
@@ -1,4 +1,20 @@
 package com.microservices.bookstore.order_service.dtos;
 
-public record OrderDTO() {
+import com.microservices.bookstore.order_service.entities.embeddable_records.Address;
+import com.microservices.bookstore.order_service.entities.embeddable_records.Customer;
+import com.microservices.bookstore.order_service.entities.enums.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record OrderDTO(
+        String orderNumber,
+        String user,
+        Set<OrderItemDto> items,
+        Customer customer,
+        Address deliveryAddress,
+        OrderStatus status,
+        String comments,
+        LocalDateTime createdAt
+) {
 }
