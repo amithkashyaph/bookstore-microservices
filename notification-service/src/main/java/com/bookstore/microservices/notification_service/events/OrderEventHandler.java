9d1982cc30ff3ffdc2380717de03520463970c1c
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 14:36:09 +0530

    added handler for order cancelled event notification

--- notification-service/src/main/java/com/bookstore/microservices/notification_service/events/OrderEventHandler.java
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/events/OrderEventHandler.java
@@ -1,5 +1,6 @@
 package com.bookstore.microservices.notification_service.events;
 
+import com.bookstore.microservices.notification_service.entities.OrderCancelledEvent;
 import com.bookstore.microservices.notification_service.entities.OrderCreatedEvent;
 import com.bookstore.microservices.notification_service.entities.OrderDeliveredEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -15,6 +16,11 @@ public class OrderEventHandler {
 
     @RabbitListener(queues = "${notifications.delivered-orders-queue}")
     void handleOrderDeliveredEvent(OrderDeliveredEvent event) {
-        System.out.println("Order created event : " + event);
+        System.out.println("Order delivered event : " + event);
+    }
+
+    @RabbitListener(queues = "${notifications.cancelled-orders-queue}")
+    void handleOrderCancelledEvent(OrderCancelledEvent event) {
+        System.out.println("Order cancelled event : " + event);
     }
 }
