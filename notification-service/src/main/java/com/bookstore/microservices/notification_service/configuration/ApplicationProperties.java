711be445d8dac4cbc2659e3cd621a801ad4a8777
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 14:02:17 +0530

    added app properties component

---
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/configuration/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package com.bookstore.microservices.notification_service.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "notifications")
+public record ApplicationProperties(
+        String orderEventsExchange,
+        String newOrdersQueue,
+        String deliveredOrdersQueue,
+        String cancelledOrdersQueue,
+        String errorOrdersQueue
+) {
+}
