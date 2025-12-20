da343a9f2a6f3dec4dd6a2123094906594067da1
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 14:58:02 +0530

    added initial order event entity

--- order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderEventEntity.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/entities/OrderEventEntity.java
@@ -1,4 +1,37 @@
 package com.microservices.bookstore.order_service.entities;
 
+import com.microservices.bookstore.order_service.entities.enums.OrderEventType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "order_events")
 public class OrderEventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String orderNumber;
+
+    @Column(nullable = false, unique = true)
+    private String eventId;
+
+    @Enumerated(EnumType.STRING)
+    private OrderEventType eventType;
+
+    @Column(nullable = false)
+    private String payload;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
