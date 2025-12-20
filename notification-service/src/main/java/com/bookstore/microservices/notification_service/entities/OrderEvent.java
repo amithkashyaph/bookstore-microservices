75549a9eca34c5482ff4e87bd87e6fc8178f43f8
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 14:59:38 +0530

    added initial order event entity

---
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/entities/OrderEvent.java
@@ -0,0 +1,13 @@
+package com.bookstore.microservices.notification_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_events")
+public class OrderEvent {
+
+}
