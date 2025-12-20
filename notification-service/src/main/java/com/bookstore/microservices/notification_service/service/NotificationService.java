ae642990d9cb25ef3de35fe1ca02c12f45d5c248
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 14:27:36 +0530

    initialized notification service component

--- notification-service/src/main/java/com/bookstore/microservices/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/service/NotificationService.java
@@ -1,7 +1,21 @@
 package com.bookstore.microservices.notification_service.service;
 
+import com.bookstore.microservices.notification_service.configuration.ApplicationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
+
+    private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
+
+    private final ApplicationProperties applicationProperties;
+    private final JavaMailSender javaMailSender;
+
+    public NotificationService(ApplicationProperties properties, JavaMailSender mailSender) {
+        this.applicationProperties = properties;
+        this.javaMailSender = mailSender;
+    }
 }
