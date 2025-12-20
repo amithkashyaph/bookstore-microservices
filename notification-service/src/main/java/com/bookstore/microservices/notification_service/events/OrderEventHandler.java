6d9179ecb99f7fd620922b79ac6bae04b36ddc6f
amithkashyap <amithkashyaprv@gmail.com>
2025-12-20 14:34:08 +0530

    added handler for order creation event notification

--- notification-service/src/main/java/com/bookstore/microservices/notification_service/events/OrderEventHandler.java
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/events/OrderEventHandler.java
@@ -1,7 +1,14 @@
 package com.bookstore.microservices.notification_service.events;
 
+import com.bookstore.microservices.notification_service.entities.OrderCreatedEvent;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventHandler {
+
+    @RabbitListener(queues = "${notifications.new-orders-queue}")
+    void handleOrderCreatedEvent(OrderCreatedEvent event) {
+        System.out.println("Order created event : " + event);
+    }
 }
