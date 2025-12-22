99494fee49e11acbac04d58894abe57b67531e96
amithkashyap <amithkashyaprv@gmail.com>
2025-12-22 13:29:01 +0530

    implemented listeners for testing purpose

--- order-service/src/main/java/com/microservices/bookstore/order_service/controllers/RabbitMQListener.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/controllers/RabbitMQListener.java
@@ -1,4 +1,18 @@
 package com.microservices.bookstore.order_service.controllers;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RabbitMQListener {
+
+    @RabbitListener(queues = "${orders.new-orders-queue}")
+    public void handleNewOrders(MyPayload payload) {
+        System.out.println("New order: " + payload.content());
+    }
+
+    @RabbitListener(queues = "${orders.delivered-orders-queue}")
+    public void handleDeliveredOrders(MyPayload payload) {
+        System.out.println("Order delivered: " + payload.content());
+    }
 }
