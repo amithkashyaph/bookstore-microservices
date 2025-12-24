425967346b03730443303ba2e4951d71ae0842a4
amithkashyap <amithkashyaprv@gmail.com>
2025-12-24 20:11:20 +0530

    added initial global exception handler

---
+++ catalog-service/src/main/java/com/microservices/bookstore/catalog_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.microservices.bookstore.catalog_service.exceptions;
+
+import java.net.URI;
+import java.time.Instant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final URI NOT_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/not-found");
+    private static final URI ISE_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/server-error");
+    private static final URI BAD_REQUEST_TYPE = URI.create("https://api.bookstore.com/errors/bad-request");
+    private static final String SERVICE_NAME = "catalog-service";
+
+
+}
