bc844bf5347f242ec0e2b1e8bc15cd94ff1ddd9e
amithkashyap <amithkashyaprv@gmail.com>
2025-12-26 23:02:59 +0530

    added order cancelled record

---
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/entities/OrderCancelledEvent.java
@@ -0,0 +1,15 @@
+package com.bookstore.microservices.notification_service.entities;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record OrderCancelledEvent(
+        String eventId,
+        String orderNumber,
+        Set<OrderItemDto> items,
+        Customer customer,
+        Address deliveryAddress,
+        String reason,
+        LocalDateTime createdAt
+) {
+}
