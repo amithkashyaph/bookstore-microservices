0f677dba8dc5536ce4fd02212016f9778f0bf9d5
amithkashyap <amithkashyaprv@gmail.com>
2025-12-27 22:40:43 +0530

    added order delivered event record

--- order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderDeliveredEvent.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderDeliveredEvent.java
@@ -1,4 +1,14 @@
 package com.microservices.bookstore.order_service.dtos;
 
-public record OrderDeliveredEvent() {
-}
+import com.microservices.bookstore.order_service.entities.embeddable_records.Address;
+import com.microservices.bookstore.order_service.entities.embeddable_records.Customer;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record OrderDeliveredEvent(
+        String eventId,
+        String orderNumber,
+        Set<OrderItemDto> items,
+        Customer customer,
+        Address deliveryAddress,
+        LocalDateTime createdAt) {}
