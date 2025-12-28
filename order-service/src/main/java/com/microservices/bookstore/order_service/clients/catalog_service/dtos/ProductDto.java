37c339c25d163bc8d365cfa4e7ba7c6de79fc36e
amithkashyap <amithkashyaprv@gmail.com>
2025-12-28 23:33:16 +0530

    formatting changes

--- order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/dtos/ProductDto.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/clients/catalog_service/dtos/ProductDto.java
@@ -2,11 +2,4 @@ package com.microservices.bookstore.order_service.clients.catalog_service.dtos;
 
 import java.math.BigDecimal;
 
-public record ProductDto(
-        String code,
-        String name,
-        String description,
-        String imageUrl,
-        BigDecimal price
-) {
-}
+public record ProductDto(String code, String name, String description, String imageUrl, BigDecimal price) {}
