d3cfebd88e607409c972fb6ea7581fe5bab9b6b2
amithkashyap <amithkashyaprv@gmail.com>
2025-12-30 21:53:32 +0530

    formatting changes

--- order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderItemDto.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/dtos/OrderItemDto.java
@@ -3,13 +3,10 @@ package com.microservices.bookstore.order_service.dtos;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import java.math.BigDecimal;
 
 public record OrderItemDto(
         @NotBlank(message = "Code is required") String code,
         @NotBlank(message = "Name is required") String name,
         @NotNull(message = "Price is required") BigDecimal price,
-        @NotNull(message = "Quantity is required") @Min(value = 1, message = "Min quantity must be 1") Integer quantity
-) {
-}
+        @NotNull(message = "Quantity is required") @Min(value = 1, message = "Min quantity must be 1") Integer quantity) {}
