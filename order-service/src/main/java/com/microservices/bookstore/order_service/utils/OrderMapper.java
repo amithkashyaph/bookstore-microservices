40bd1c794e55200e11d3df453e3ccbcae30e4d01
amithkashyap <amithkashyaprv@gmail.com>
2025-12-30 21:54:04 +0530

    added convert to dto helper method

--- order-service/src/main/java/com/microservices/bookstore/order_service/utils/OrderMapper.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/utils/OrderMapper.java
@@ -1,14 +1,15 @@
 package com.microservices.bookstore.order_service.utils;
 
 import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
+import com.microservices.bookstore.order_service.dtos.OrderDTO;
 import com.microservices.bookstore.order_service.dtos.OrderItemDto;
 import com.microservices.bookstore.order_service.entities.Order;
 import com.microservices.bookstore.order_service.entities.OrderItem;
 import com.microservices.bookstore.order_service.entities.enums.OrderStatus;
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class OrderMapper {
     public static Order convertToEntity(CreateOrderRequest request) {
@@ -31,19 +32,19 @@ public class OrderMapper {
         return newOrder;
     }
 
-//    static OrderDTO convertToDTO(OrderEntity order) {
-//        Set<OrderItem> orderItems = order.getItems().stream()
-//                .map(item -> new OrderItem(item.getCode(), item.getName(), item.getPrice(), item.getQuantity()))
-//                .collect(Collectors.toSet());
-//
-//        return new OrderDTO(
-//                order.getOrderNumber(),
-//                order.getUserName(),
-//                orderItems,
-//                order.getCustomer(),
-//                order.getDeliveryAddress(),
-//                order.getStatus(),
-//                order.getComments(),
-//                order.getCreatedAt());
-//    }
+    public static OrderDTO convertToDTO(Order order) {
+        Set<OrderItemDto> orderItems = order.getOrderItems().stream()
+                .map(item -> new OrderItemDto(item.getCode(), item.getName(), item.getPrice(), item.getQuantity()))
+                .collect(Collectors.toSet());
+
+        return new OrderDTO(
+                order.getOrderNumber(),
+                order.getUserName(),
+                orderItems,
+                order.getCustomer(),
+                order.getDeliveryAddress(),
+                order.getStatus(),
+                order.getComments(),
+                order.getCreatedAt());
+    }
 }
