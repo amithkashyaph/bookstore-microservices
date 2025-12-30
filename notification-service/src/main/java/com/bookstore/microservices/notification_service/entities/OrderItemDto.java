9381071a1914d349832c4aba87a4109c9e0c27f3
amithkashyap <amithkashyaprv@gmail.com>
2025-12-30 21:52:57 +0530

    added initial order item dto

---
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/entities/OrderItemDto.java
@@ -0,0 +1,12 @@
+package com.bookstore.microservices.notification_service.entities;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record OrderItemDto(
+
+) {
+}
