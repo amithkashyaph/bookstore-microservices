dddd727d4cf633beb4962cce0472fd32e6f26e14
amithkashyap <amithkashyaprv@gmail.com>
2026-01-01 23:19:50 +0530

    formatting changes

--- order-service/src/main/java/com/microservices/bookstore/order_service/repositories/OrderEventRepository.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/repositories/OrderEventRepository.java
@@ -3,5 +3,4 @@ package com.microservices.bookstore.order_service.repositories;
 import com.microservices.bookstore.order_service.entities.OrderEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderEventRepository extends JpaRepository<OrderEventEntity, Long> {
-}
+public interface OrderEventRepository extends JpaRepository<OrderEventEntity, Long> {}
