80d9f95921bbace0a157b871317c2869a665ad7d
amithkashyap <amithkashyaprv@gmail.com>
2026-01-02 22:31:11 +0530

    added initial mapper method

---
+++ catalog-service/src/main/java/com/microservices/bookstore/catalog_service/utils/ProductEntityMapper.java
@@ -0,0 +1,10 @@
+package com.microservices.bookstore.catalog_service.utils;
+
+import com.microservices.bookstore.catalog_service.dtos.ProductResponseDTO;
+import com.microservices.bookstore.catalog_service.entities.Product;
+
+public class ProductEntityMapper {
+
+    public static ProductResponseDTO toProductResponseDTO(Product product) {
+
+}
