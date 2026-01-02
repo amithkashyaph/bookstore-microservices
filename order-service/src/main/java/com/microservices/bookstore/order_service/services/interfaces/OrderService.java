97611bc44fd99d562a17857dd1f82c0315b58966
amithkashyap <amithkashyaprv@gmail.com>
2026-01-02 22:28:13 +0530

    added fetch order info for user

--- order-service/src/main/java/com/microservices/bookstore/order_service/services/interfaces/OrderService.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/services/interfaces/OrderService.java
@@ -1,13 +1,12 @@
 package com.microservices.bookstore.order_service.services.interfaces;
 
-import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
-import com.microservices.bookstore.order_service.dtos.CreateOrderResponse;
-import com.microservices.bookstore.order_service.dtos.OrderSummary;
-
+import com.microservices.bookstore.order_service.dtos.*;
 import java.util.List;
 
 public interface OrderService {
     CreateOrderResponse createOrder(String username, CreateOrderRequest createOrderRequest);
+
     List<OrderSummary> fetchAllOrdersForUser(String username);
 
+    OrderDTO fetchOrderInfoForUserAndOrderNumber(String username, String orderNumber);
 }
