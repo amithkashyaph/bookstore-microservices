2d12e55020d8f77e39cec31f72233b1a0f21194a
amithkashyap <amithkashyaprv@gmail.com>
2026-01-03 23:37:11 +0530

    added process new orders job

--- order-service/src/main/java/com/microservices/bookstore/order_service/jobs/OrderProcessingJob.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/jobs/OrderProcessingJob.java
@@ -1,18 +1,29 @@
 package com.microservices.bookstore.order_service.jobs;
 
-import com.microservices.bookstore.order_service.services.OrderEventServiceImpl;
+import com.microservices.bookstore.order_service.services.OrderServiceImpl;
+import java.time.Instant;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderProcessingJob {
     private static final Logger log = LoggerFactory.getLogger(OrderProcessingJob.class);
 
-    private final OrderEventServiceImpl orderEventService;
+    private final OrderServiceImpl orderService;
 
-    public OrderProcessingJob(OrderEventServiceImpl orderEventService) {
-        this.orderEventService = orderEventService;
+    public OrderProcessingJob(OrderServiceImpl orderService) {
+        this.orderService = orderService;
     }
 
+    @Scheduled(cron = "${orders.new-orders-job-cron}")
+    @SchedulerLock(name = "processNewOrders")
+    public void processNewOrders() {
+        log.info("Processing new orders at {}", Instant.now());
+        LockAssert.assertLocked();
+        orderService.processNewOrders();
+    }
 }
