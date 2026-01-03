535e919a5566d4feb6a5fd31e6fc4026dd198ba7
amithkashyap <amithkashyaprv@gmail.com>
2026-01-03 23:38:23 +0530

    added initial unit test class

---
+++ order-service/src/test/java/com/microservices/bookstore/order_service/controllers/OrderControllerUnitTests.java
@@ -0,0 +1,42 @@
+package com.microservices.bookstore.order_service.controllers;
+
+import static com.microservices.bookstore.order_service.testdata.TestDataFactory.*;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
+import com.microservices.bookstore.order_service.services.SecurityService;
+import com.microservices.bookstore.order_service.services.interfaces.OrderService;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerUnitTests {
+    @MockitoBean
+    private OrderService orderService;
+
+    @MockitoBean
+    private SecurityService securityService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+}
