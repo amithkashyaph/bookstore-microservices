505ff16846440b1a86020cb317baa72b32b30e5f
amithkashyap <amithkashyaprv@gmail.com>
2026-01-04 22:46:46 +0530

    implemented product dto conversion logic

--- catalog-service/src/main/java/com/microservices/bookstore/catalog_service/utils/ProductEntityMapper.java
+++ catalog-service/src/main/java/com/microservices/bookstore/catalog_service/utils/ProductEntityMapper.java
@@ -6,5 +6,11 @@ import com.microservices.bookstore.catalog_service.entities.Product;
 public class ProductEntityMapper {
 
     public static ProductResponseDTO toProductResponseDTO(Product product) {
-
+        return new ProductResponseDTO(
+                product.getCode(),
+                product.getName(),
+                product.getDescription(),
+                product.getImageUrl(),
+                product.getPrice());
+    }
 }
