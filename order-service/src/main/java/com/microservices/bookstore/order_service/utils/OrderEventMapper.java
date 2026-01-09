7219df3f515cceb69a40b07f4d52b72251feef80
amithkashyap <amithkashyaprv@gmail.com>
2026-01-09 23:34:32 +0530

    added order created event mapper method

--- order-service/src/main/java/com/microservices/bookstore/order_service/utils/OrderEventMapper.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/utils/OrderEventMapper.java
@@ -1,14 +1,21 @@
 package com.microservices.bookstore.order_service.utils;
 
-import com.microservices.bookstore.order_service.dtos.OrderCreatedEvent;
-import com.microservices.bookstore.order_service.dtos.OrderItemDto;
+import com.microservices.bookstore.order_service.dtos.*;
 import com.microservices.bookstore.order_service.entities.Order;
-
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class OrderEventMapper {
+    public static OrderCreatedEvent buildOrderCreatedEvent(Order order) {
+        return new OrderCreatedEvent(
+                UUID.randomUUID().toString(),
+                order.getOrderNumber(),
+                getOrderItems(order),
+                order.getCustomer(),
+                order.getDeliveryAddress(),
+                LocalDateTime.now());
+    }
 
 }
