0cc47f947fc3b83057d5366f75199890d2aff4d1
amithkashyap <amithkashyaprv@gmail.com>
2026-01-15 22:01:00 +0530

    refactored variable names

--- notification-service/src/main/java/com/bookstore/microservices/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/service/NotificationService.java
@@ -1,9 +1,15 @@
 package com.bookstore.microservices.notification_service.service;
 
 import com.bookstore.microservices.notification_service.configuration.ApplicationProperties;
+import com.bookstore.microservices.notification_service.entities.OrderCancelledEvent;
+import com.bookstore.microservices.notification_service.entities.OrderCreatedEvent;
+import com.bookstore.microservices.notification_service.entities.OrderDeliveredEvent;
+import com.bookstore.microservices.notification_service.entities.OrderErrorEvent;
+import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +17,11 @@ public class NotificationService {
 
     private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
 
-    private final ApplicationProperties applicationProperties;
-    private final JavaMailSender javaMailSender;
+    private final ApplicationProperties properties;
+    private final JavaMailSender emailSender;
 
     public NotificationService(ApplicationProperties properties, JavaMailSender mailSender) {
-        this.applicationProperties = properties;
-        this.javaMailSender = mailSender;
+        this.properties = properties;
+        this.emailSender = mailSender;
     }
 }
