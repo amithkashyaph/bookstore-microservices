f551a8cad39bd6b4c2a426eb74ac6f60a1a0e5b4
amithkashyap <amithkashyaprv@gmail.com>
2026-01-15 21:58:02 +0530

    added cors mapping

--- catalog-service/src/main/java/com/microservices/bookstore/catalog_service/config/WebMvcConfig.java
+++ catalog-service/src/main/java/com/microservices/bookstore/catalog_service/config/WebMvcConfig.java
@@ -1,4 +1,18 @@
 package com.microservices.bookstore.catalog_service.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false);
+    }
 }
