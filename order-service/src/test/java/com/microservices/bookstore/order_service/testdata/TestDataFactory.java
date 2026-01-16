b6a4c750e55bf7e0ffebf033555610e925a84ec9
amithkashyap <amithkashyaprv@gmail.com>
2026-01-16 21:21:17 +0530

    added initial test data factory class

---
+++ order-service/src/test/java/com/microservices/bookstore/order_service/testdata/TestDataFactory.java
@@ -0,0 +1,16 @@
+package com.microservices.bookstore.order_service.testdata;
+
+import static org.instancio.Select.field;
+
+import com.microservices.bookstore.order_service.dtos.CreateOrderRequest;
+import com.microservices.bookstore.order_service.dtos.OrderItemDto;
+import com.microservices.bookstore.order_service.entities.embeddable_records.Address;
+import com.microservices.bookstore.order_service.entities.embeddable_records.Customer;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+import org.instancio.Instancio;
+
+public class TestDataFactory {
+
+}
