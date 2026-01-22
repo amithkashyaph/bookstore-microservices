8ed130001191324a190c15c894de59dc4ef75fc6
amithkashyap <amithkashyaprv@gmail.com>
2026-01-22 23:43:28 +0530

    added exchange bean

--- order-service/src/main/java/com/microservices/bookstore/order_service/configuration/RabbitMQConfig.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/configuration/RabbitMQConfig.java
@@ -1,13 +1,26 @@
 package com.microservices.bookstore.order_service.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
-    ApplicationProperties applicationProperties;
+    private ApplicationProperties applicationProperties;
 
     public RabbitMQConfig(ApplicationProperties applicationProperties) {
         this.applicationProperties = applicationProperties;
     }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(applicationProperties.orderEventsExchange());
+    }
+
+
 }
