435cfecb5b0fcaa014cf10ea27857499a76608c8
amithkashyap <amithkashyaprv@gmail.com>
2026-01-23 23:28:34 +0530

    added delivered orders queue bean

--- order-service/src/main/java/com/microservices/bookstore/order_service/configuration/RabbitMQConfig.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/configuration/RabbitMQConfig.java
@@ -32,4 +32,10 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(newOrdersQueue()).to(exchange()).with(applicationProperties.newOrdersQueue());
     }
 
+    @Bean
+    Queue deliveredOrdersQueue() {
+        return QueueBuilder.durable(applicationProperties.deliveredOrdersQueue())
+                .build();
+    }
+
 }
