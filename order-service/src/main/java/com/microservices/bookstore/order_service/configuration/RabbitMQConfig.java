12fd25aa18b8b79ec399bba12583bb46407a2c96
amithkashyap <amithkashyaprv@gmail.com>
2026-01-31 23:53:35 +0530

    added cancelled orders queue binding bean

--- order-service/src/main/java/com/microservices/bookstore/order_service/configuration/RabbitMQConfig.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/configuration/RabbitMQConfig.java
@@ -51,4 +51,11 @@ public class RabbitMQConfig {
                 .build();
     }
 
+    @Bean
+    Binding cancelledOrdersQueueBinding() {
+        return BindingBuilder.bind(cancelledOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.cancelledOrdersQueue());
+    }
+
 }
