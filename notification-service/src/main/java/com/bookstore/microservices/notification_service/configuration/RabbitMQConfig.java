5b035a55976c597fd34146c5e6165806cb29b8e4
amithkashyap <amithkashyaprv@gmail.com>
2026-02-07 23:49:10 +0530

    added jackson converter bean

--- notification-service/src/main/java/com/bookstore/microservices/notification_service/configuration/RabbitMQConfig.java
+++ notification-service/src/main/java/com/bookstore/microservices/notification_service/configuration/RabbitMQConfig.java
@@ -69,6 +69,11 @@ public class RabbitMQConfig {
         return rabbitTemplate;
     }
 
+    @Bean
+    public Jackson2JsonMessageConverter jacksonConverter(ObjectMapper mapper) {
+        return new Jackson2JsonMessageConverter(mapper);
+    }
+
 
 
 }
