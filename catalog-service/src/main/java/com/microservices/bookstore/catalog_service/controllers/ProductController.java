abbcbf2e7b09d10f6ab09625f977a9e65444f01d
amithkashyap <amithkashyaprv@gmail.com>
2026-02-08 23:37:15 +0530

    added initial product controller component

---
+++ catalog-service/src/main/java/com/microservices/bookstore/catalog_service/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.microservices.bookstore.catalog_service.controllers;
+
+import com.microservices.bookstore.catalog_service.dtos.PagedResult;
+import com.microservices.bookstore.catalog_service.dtos.ProductResponseDTO;
+import com.microservices.bookstore.catalog_service.services.interfaces.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+class ProductController {
+
+}
