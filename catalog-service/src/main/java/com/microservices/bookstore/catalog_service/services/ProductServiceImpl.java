a2968c02289cba9f583bdace60299400ae356be7
amithkashyap <amithkashyaprv@gmail.com>
2026-02-11 22:43:41 +0530

    added initial service impl component

---
+++ catalog-service/src/main/java/com/microservices/bookstore/catalog_service/services/ProductServiceImpl.java
@@ -0,0 +1,21 @@
+package com.microservices.bookstore.catalog_service.services;
+
+import com.microservices.bookstore.catalog_service.config.ApplicationProperties;
+import com.microservices.bookstore.catalog_service.dtos.PagedResult;
+import com.microservices.bookstore.catalog_service.dtos.ProductResponseDTO;
+import com.microservices.bookstore.catalog_service.entities.Product;
+import com.microservices.bookstore.catalog_service.exceptions.ProductNotFoundException;
+import com.microservices.bookstore.catalog_service.repositories.ProductRepository;
+import com.microservices.bookstore.catalog_service.services.interfaces.ProductService;
+import com.microservices.bookstore.catalog_service.utils.ProductEntityMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+
+}
