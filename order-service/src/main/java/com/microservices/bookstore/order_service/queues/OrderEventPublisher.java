401b768f0b1a3bb90214895c6785ec7dee7f82d9
amithkashyap <amithkashyaprv@gmail.com>
2026-02-16 22:34:31 +0530

    implemented publish order created event

--- order-service/src/main/java/com/microservices/bookstore/order_service/queues/OrderEventPublisher.java
+++ order-service/src/main/java/com/microservices/bookstore/order_service/queues/OrderEventPublisher.java
@@ -1,12 +1,19 @@
 package com.microservices.bookstore.order_service.queues;
 
 import com.microservices.bookstore.order_service.configuration.ApplicationProperties;
+import com.microservices.bookstore.order_service.dtos.OrderCancelledEvent;
 import com.microservices.bookstore.order_service.dtos.OrderCreatedEvent;
+import com.microservices.bookstore.order_service.dtos.OrderDeliveredEvent;
+import com.microservices.bookstore.order_service.dtos.OrderErrorEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventPublisher {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderEventPublisher.class);
 
     private final RabbitTemplate rabbitTemplate;
     private final ApplicationProperties applicationProperties;
@@ -15,6 +22,9 @@ public class OrderEventPublisher {
         this.rabbitTemplate = rabbitTemplate;
         this.applicationProperties = applicationProperties;
     }
+
     public void publish(OrderCreatedEvent orderCreatedEvent) {
+        log.info("routing key used {}", applicationProperties.newOrdersQueue());
+        this.send(applicationProperties.newOrdersQueue(), orderCreatedEvent);
     }
 }
